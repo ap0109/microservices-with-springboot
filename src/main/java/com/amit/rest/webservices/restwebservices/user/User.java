041120a316fb29e8fd790b72fda160c80999f050
ap0109 <amitporia132@gmail.com>
2020-08-30 20:26:28 +0930

    Added Validation, Hateoas, internationalization and content negotiation features

--- src/main/java/com/amit/rest/webservices/restwebservices/user/User.java
+++ src/main/java/com/amit/rest/webservices/restwebservices/user/User.java
@@ -2,12 +2,17 @@ package com.amit.rest.webservices.restwebservices.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 	
 	private Integer id;
 	
+	@Size(min=2, message="Name should have alteast 2 characters")
 	private String name;
 	
+	@Past
 	private Date birthdate;
 	
 	protected User () {
--- src/main/java/com/amit/rest/webservices/restwebservices/user/UserResource.java
+++ src/main/java/com/amit/rest/webservices/restwebservices/user/UserResource.java
@@ -1,35 +1,72 @@
 package com.amit.rest.webservices.restwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserResource {
 
 	@Autowired
 	private UserDaoService daoService;
-	
+
 	@GetMapping("/users")
-	public List<User> retrieveAllUsers(){
+	public List<User> retrieveAllUsers() {
 		return daoService.findAll();
 	}
-	
+
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id){
-		return daoService.findOne(id);
+	public EntityModel<User> retrieveUser(@PathVariable int id) {
+		User user = daoService.findOne(id);
+		if (user == null) {
+			throw new UserNotFoundException("id-" + id);
+		}
+		
+		// "all-users" SERVER_PATH + "/users"
+		// retrieveAllUsers
+		EntityModel<User> resource = EntityModel.of(user);
+
+		WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+		resource.add(linkTo.withRel("all-users"));
+		// HATEOAS
+
+		return resource;
+
 	}
-	
+
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = daoService.deleteById(id);
+		if (user == null) {
+			throw new UserNotFoundException("id-" + id);
+		}
+	}
+
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user){
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		User savedUser = daoService.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+
+		return ResponseEntity.created(location).build();
 	}
-	
-	
 
 }
